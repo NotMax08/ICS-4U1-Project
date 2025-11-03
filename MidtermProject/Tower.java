d9e5f745d98f304baebd460fcbfbf26869abfe7b
Julianx299 <348590738@gapps.yrdsb.ca>
2025-11-03 09:09:19 -0500

    Tower

---
+++ MidtermProject/Tower.java
@@ -0,0 +1,24 @@
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList;
+/**
+ * Write a description of class Tower here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public abstract class Tower extends SuperSmoothMover
+{
+    protected int damage;
+    
+    public Tower()
+    {
+        
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+    
+    
+}
