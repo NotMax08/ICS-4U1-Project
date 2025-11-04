e0e956a245447b3e4fb06aa5314b29d50cc0c405
crimsoo-n <robinnoodless@gmail.com>
2025-11-04 09:24:33 -0500

    balon

--- MidtermProject/Balloon.java
+++ MidtermProject/Balloon.java
@@ -5,6 +5,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  *  Balloons float across the path and try to reach the end of the path
  *  Balloons vary by colour, health, speed, states
  * 
+ *  Art Credits:
+ *  Red Balloon - PNG Tree
+ *  Pop Image - @Color Optimist on OpenGameArt.org
+ *  
  * @author Robin Liu 
  * @version Nov 3
  */
@@ -14,42 +18,69 @@ public abstract class Balloon extends SuperSmoothMover
     protected double speed; 
     protected int weightValue; //how heavy the balloon is ?
     protected GreenfootImage image;
+    protected static GreenfootImage popImage = new GreenfootImage("popImage.png");
+    protected boolean popped;
     protected int actCount; //counts how long the balloon has lived
-    
+
     // special balloon states 
     protected boolean camo; 
     protected boolean lead;
-    
-    
+
     public Balloon(){
-        
+        enableStaticRotation();
+        popImage.scale(50,50);
     }
-    
+
     public void act(){
         actCount++;
-        move();
+        if(popped){
+            if(actCount == actCount + 20){
+                removeMe();
+            }else{
+                popMe();
+            }
+        }else{
+            move();
+        }
     }
-    
+
     public void move(){
         move(speed);
     }
-    
+
     public int getActCount(){
         return actCount;
     }
-    
+
+    /**
+     *  Method to damage the balloon
+     */
+    public void damageMe(int damage){
+        if(HP - damage <= 0){
+            popped = true;
+            //pop sound;
+        }else{
+            this.HP = HP - damage;
+        }
+    }
+
     /**
      *  Methods to get the state of the ballooon
      */
     public boolean isCamo(){
         return camo;
     }
+
     public boolean isLead(){
         return lead;
     }
-    
+
     public void removeMe(){
         getWorld().removeObject(this);
     }
-    
+
+    public void popMe(){
+        setImage(popImage);
+    }
+
 }
---
+++ MidtermProject/GreenBalloon.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GreenBalloon here.
+ * 
+ * @author Robin Liu
+ * @version Nov 4
+ */
+public class GreenBalloon extends Balloon
+{
+    
+    
+    public GreenBalloon(){
+        HP = 5;
+        speed = 5;
+        weightValue = 3;
+        drawImage();
+    }
+    
+    public void act()
+    {
+        super.act();
+    }
+    
+    public void drawImage(){
+        image = new GreenfootImage("GreenBalloon.png");
+        image.scale(60,50);
+        setImage(image);
+    }
+}
