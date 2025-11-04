5c63b0d6e39d73a19e7dee9598a85383e4bb6fb7
Julianx299 <348590738@gapps.yrdsb.ca>
2025-11-04 08:21:01 -0500

    pioupouo

--- MidtermProject/Tower.java
+++ MidtermProject/Tower.java
@@ -11,7 +11,7 @@ public abstract class Tower extends SuperSmoothMover
     protected int damage; //Damage of tower
     protected ArrayList<Balloon> targets; //List of all targets within Tower's range
     protected Balloon closestTarget; //Stores the closest target
-    protected int balloonAge;
+    protected int greatestBalloonAge;
     protected int range; //Tower range
     protected GreenfootImage image;
     
@@ -28,16 +28,16 @@ public abstract class Tower extends SuperSmoothMover
     
     public void getClosestTarget()
     {
-        balloonAge = 0;
+        greatestBalloonAge = 0;
         targets = (ArrayList<Balloon>)getObjectsInRange(range, Balloon.class);
         
         if(targets != null)
         {
             for (Balloon b: targets)
             {
-                if(b.getActCount() > balloonAge)
+                if(b.getActCount() > greatestBalloonAge)
                 {
-                    
+                    greatestBalloonAge = b.getActCount();
                 }
             }
         }
